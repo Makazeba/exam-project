25c65c192effa7b33ff5db35272da2f865fbe844
Makazeba <makazebaredmi@gmail.com>
2022-08-25 15:56:23 +0300

    modal

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    html: document.querySelector("html")
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,21 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.html.classList.toggle("overflow-lock");
+  }
+
+  const refs2 = {
+    openModalBtn: document.querySelector("[data-modal2-open]"),
+    closeModalBtn: document.querySelector("[data-modal2-close]"),
+    modal: document.querySelector("[data-modal2]"),
+    html: document.querySelector("html")
+  };
+
+  refs2.openModalBtn.addEventListener("click", toggleModal2);
+  refs2.closeModalBtn.addEventListener("click", toggleModal2);
+
+  function toggleModal2() {
+    refs2.modal.classList.toggle("is-hidden");
+    refs.html.classList.toggle("overflow-lock");
   }
 })();
